ef6e38b56f0894f509dc2951580a1d753391f9f8
Jeong Jinwoo <jwjeong96@gmail.com>
2022-02-02 13:53:22 +0900

    fix(usePopup): remove isPopupMode from useEffect dependency to end infinite call

--- src/hooks/usePopup.ts
+++ src/hooks/usePopup.ts
@@ -18,6 +18,6 @@ export const usePopup = ({
         setIsPopupMode(false);
       }, 3000);
       return () => clearTimeout(closePopUp); // return a function in the useEffect callback and that function will run when the component unmounts
-    }
-  }, [isFinished, isPopupMode, setIsPopupMode]);
+    } // eslint-disable-next-line
+  }, [isFinished, setIsPopupMode]); // isPopupMode을 추가하는 경우 팝업 모드가 false가 될때마다 재호출되면서 절대 안 닫힘
 };
